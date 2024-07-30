44d75f5cd455ed7b57b7a2f55787857db4022663
Roberto Lliguin <166523151+RCL05@users.noreply.github.com>
2024-07-30 09:58:47 -0500

    Create Supervisor.java
    Clase 15

---
+++ Supervisor.java
@@ -0,0 +1,17 @@
+public class Supervisor {
+    private int idSupervisor;
+
+    public boolean supervisarJuego() {
+        // Implementación del método
+        return true;
+    }
+
+    // Getters y setters
+    public int getIdSupervisor() {
+        return idSupervisor;
+    }
+
+    public void setIdSupervisor(int idSupervisor) {
+        this.idSupervisor = idSupervisor;
+    }
+}
