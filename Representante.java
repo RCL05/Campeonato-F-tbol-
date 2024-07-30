7b76d82c8f1901c6a6a4b2802cebf9032e8cbb48
Roberto Lliguin <166523151+RCL05@users.noreply.github.com>
2024-07-30 09:48:58 -0500

    Create Representante.java
    Clase 5

---
+++ Representante.java
@@ -0,0 +1,30 @@
+public class Representante {
+    private String nombre;
+    private String colorUniforme;
+    private int idEquipo;
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColorUniforme() {
+        return colorUniforme;
+    }
+
+    public void setColorUniforme(String colorUniforme) {
+        this.colorUniforme = colorUniforme;
+    }
+
+    public int getIdEquipo() {
+        return idEquipo;
+    }
+
+    public void setIdEquipo(int idEquipo) {
+        this.idEquipo = idEquipo;
+    }
+}
