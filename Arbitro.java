38a76fe4f2c9d450600d3f8fb3f4686078690846
Roberto Lliguin <166523151+RCL05@users.noreply.github.com>
2024-07-30 09:57:27 -0500

    Create Arbitro.java
    Clase 13

---
+++ Arbitro.java
@@ -0,0 +1,16 @@
+public class Arbitro {
+    private int idArbitro;
+
+    public void pitarJuego() {
+        // Implementación del método
+    }
+
+    // Getters y setters
+    public int getIdArbitro() {
+        return idArbitro;
+    }
+
+    public void setIdArbitro(int idArbitro) {
+        this.idArbitro = idArbitro;
+    }
+}
