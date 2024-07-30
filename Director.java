7f7b83205e416e7b1480bd8d033d4698b44e09f4
Roberto Lliguin <166523151+RCL05@users.noreply.github.com>
2024-07-30 09:58:01 -0500

    Create Director.java
    Clase 14

---
+++ Director.java
@@ -0,0 +1,12 @@
+public class Director {
+    private int idDirector;
+
+    // Getters y setters
+    public int getIdDirector() {
+        return idDirector;
+    }
+
+    public void setIdDirector(int idDirector) {
+        this.idDirector = idDirector;
+    }
+}
