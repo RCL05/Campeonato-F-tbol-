ea44ff23bbdde3c5919962edb20e82bf4718b652
Roberto Lliguin <166523151+RCL05@users.noreply.github.com>
2024-07-30 09:48:14 -0500

    Create InscripciondelEquipo.java
    Clase 4

---
+++ InscripciondelEquipo.java
@@ -0,0 +1,30 @@
+public class InscripciondelEquipo {
+    private int idEquipo;
+    private Equipo equipo;
+    private Representante representante;
+
+    // Getters y setters
+    public int getIdEquipo() {
+        return idEquipo;
+    }
+
+    public void setIdEquipo(int idEquipo) {
+        this.idEquipo = idEquipo;
+    }
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
+    }
+
+    public Representante getRepresentante() {
+        return representante;
+    }
+
+    public void setRepresentante(Representante representante) {
+        this.representante = representante;
+    }
+}
