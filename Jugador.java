56b4f15a4562874468ac07b16efe300dc11d514c
Roberto Lliguin <166523151+RCL05@users.noreply.github.com>
2024-07-30 09:56:53 -0500

    Create Jugador.java

---
+++ Jugador.java
@@ -0,0 +1,30 @@
+public class Jugador {
+    private String categoria;
+    private int idJugador;
+    private Persona persona;
+
+    // Getters y setters
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getIdJugador() {
+        return idJugador;
+    }
+
+    public void setIdJugador(int idJugador) {
+        this.idJugador = idJugador;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+}
